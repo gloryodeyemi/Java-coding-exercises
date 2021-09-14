2c8d6c1d0e7b5575acaa4581f0ea4dabdb53a658
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2021-09-14 19:40:19 +0100

    Point

--- src/Main.java
+++ src/Main.java
@@ -66,5 +66,15 @@ public class Main {
         System.out.println("width= " + wall.getWidth());
         System.out.println("height= " + wall.getHeight());
         System.out.println("area= " + wall.getArea());
+        System.out.println("---------------------------");
+
+        // Point
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
     }
 }
