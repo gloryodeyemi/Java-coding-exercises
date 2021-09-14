cc575d9655a6a235e717b2a47662089379e4e696
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2021-09-14 18:17:24 +0100

    Wall area

--- src/Main.java
+++ src/Main.java
@@ -58,5 +58,13 @@ public class Main {
         VipCustomer customer3 = new VipCustomer();
         System.out.println("Credit limit = " + customer3.getCreditLimit());
         System.out.println("---------------------------");
+
+        // Wall Area
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
