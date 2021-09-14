d6c16a3d97af8f1cb837f897d4694f5a373df116
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2021-09-14 21:39:19 +0100

    Complex Number

--- src/Main.java
+++ src/Main.java
@@ -87,5 +87,20 @@ public class Main {
         floor = new Floor(5.4, 4.5);
         calculator1 = new Calculator(floor, carpet);
         System.out.println("total= " + calculator1.getTotalCost());
+        System.out.println("---------------------------");
+
+        // Complex Number
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+        System.out.println("---------------------------");
     }
 }
