48d1a867d68de26e7c9f34f2813769ec72082a5a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2021-09-17 17:20:32 +0100

    Inheritance

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Vehicle, Car, and Toyota
+        Toyota toyota = new Toyota(36);
+        toyota.steer(45);
+        toyota.accelerate(30);
+        toyota.accelerate(20);
+        toyota.accelerate(-42);
+        System.out.println("---------------------------");
+
+        // Circle and Cylinder
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
+        System.out.println("---------------------------");
+
+        // Pool Area - Rectangle and Cuboid
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+        System.out.println("---------------------------");
+    }
+}
