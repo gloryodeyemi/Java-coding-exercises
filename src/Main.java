575d5968cdf5f4455f2ebe3a2d401483e9fb1432
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2021-09-17 13:22:27 +0100

    Inheritance Circle and Cylinder

--- src/Main.java
+++ src/Main.java
@@ -109,5 +109,17 @@ public class Main {
         toyota.accelerate(30);
         toyota.accelerate(20);
         toyota.accelerate(-42);
+        System.out.println("---------------------------");
+
+        // Circle and Cylinder
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
+        System.out.println("---------------------------");
     }
 }
