0beed2a94af9a5bd620436201bf05c2246a464a1
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2021-11-01 16:02:05 +0100

    Interfaces challenge

---
+++ Interfaces/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player glory = new Player("Glory", 10, 15);
+        System.out.println(glory.toString());
+        saveObject(glory);
+
+        glory.setHitPoints(8);
+        System.out.println(glory);
+        glory.setWeapon("Stormbringer");
+        saveObject(glory);
+        loadObject(glory);
+        System.out.println(glory);
+
+        ISaveable werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println(werewolf);
+        saveObject(werewolf);
+    }
+    public static ArrayList<String> readvalues(){
+        ArrayList<String> values = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.println("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave){
+        for (int i=0; i<objectToSave.write().size(); i++){
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readvalues();
+        objectToLoad.read(values);
+    }
+}
